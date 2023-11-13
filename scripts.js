58fabcf1119bc70c9c832ca3d8ace6bbc76e482c
Alberto <132847268+1a-Alberto@users.noreply.github.com>
2023-11-12 18:20:17 -0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,14 +1,1 @@
-document.getElementById('formulario').addEventListener('submit', function(event) {
-    event.preventDefault();
 
-    let nombre = document.getElementById('nombre').value;
-    let mensaje = document.getElementById('mensaje').value;
-
-    let li = document.createElement('li');
-    li.textContent = nombre + ': ' + mensaje;
-
-    document.getElementById('lista-mensajes').appendChild(li);
-
-    document.getElementById('nombre').value = '';
-    document.getElementById('mensaje').value = '';
-});
